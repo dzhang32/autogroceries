1811933194372a07e91b10828937ccdd2a1a6d04
David Zhang <dyzhang32@gmail.com>
2021-09-28 22:58:34 +0100

    add functions to login, and browse sainsburys

--- src/autoceries/browser/Browser.py
+++ src/autoceries/browser/Browser.py
@@ -1,12 +1,52 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
 class Browser():
-    def __init__(self, url, driver):
-        self._driver = driver
+    def __init__(self, url):
         self._url = url
+        self._webdriver = webdriver
+        self._webdriver_manager = ChromeDriverManager()
+        self._driver = None
+
+    @property
+    def url(self):
+        return self._url
+
+    @property
+    def webdriver(self):
+        return self._webdriver
+
+    @webdriver.setter
+    def webdriver(self, value):
+        self._webdriver = value
+
+    @property
+    def webdriver_manager(self):
+        return self._webdriver_manager
 
     @property
     def driver(self):
         return self._driver
 
-    @property
-    def url(self):
-        return self._url
+    def open_driver(self):
+        driver = self.webdriver.Chrome(self.webdriver_manager.install())
+        self._driver = driver
+
+    def open_url(self):
+        self._check_driver_is_not_none()
+        self.driver.get(self.url)
+
+    def close_driver(self):
+        self._driver.close()
+
+    def _check_driver_is_not_none(self):
+        if self.driver is None:
+            raise ValueError("driver has not yet been initialised")
+
+
+if __name__ == "__main__":
+    b = Browser("https://www.sainsburys.co.uk")
+    # b.open_driver()
+    b.open_site()
+    b.close_driver()
--- src/autoceries/browser/SainsburyBrowser.py
+++ src/autoceries/browser/SainsburyBrowser.py
@@ -1,8 +1,81 @@
-from autoceries.browser.Browser import Browser
+import time
+from autoceries.browser import Browser
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class SainsburyBrowser(Browser):
-    def __init__(self, driver):
+    def __init__(self):
         sainsbury_url = "https://www.sainsburys.co.uk"
 
-        super().__init__(sainsbury_url, driver)
+        super().__init__(sainsbury_url)
+
+    def shop(self, username, password):
+        self.open_sainsbury()
+        self.accept_cookies()
+        self.to_login()
+        self.login(username, password)
+        self.add_to_cart("tomato")
+
+    def add_to_cart(self, item):
+        search = self.driver.find_element_by_id("search-bar-input")
+        search.send_keys(item)
+        search = self.driver.find_element_by_xpath(
+            "//button[@class='search-bar__button']"
+        )
+        search.click()
+
+    def open_sainsbury(self):
+        self.open_driver()
+        self.open_url()
+
+        # wait a second for page to load
+        time.sleep(1)
+
+    def accept_cookies(self):
+        # wait for up to 10 seconds for the cookies box to be clickable
+        wait = WebDriverWait(self.driver, 5)
+        accept_box = wait.until(EC.element_to_be_clickable(
+            (By.XPATH, "//button[text()='Accept All Cookies']"))
+        )
+        accept_box.click()
+
+    def to_login(self):
+        login = self.driver.find_element_by_xpath("//span[text()='Log in']")
+        login.click()
+        groceries = self.driver.find_element_by_xpath(
+            "//a[text()='Groceries account']"
+        )
+        groceries.click()
+
+    def login(self, username, password):
+        self.accept_cookies()
+
+        # enter UN and PW
+        un = self.driver.find_element_by_id("username")
+        un.send_keys(username)
+        pw = self.driver.find_element_by_id("password")
+        pw.send_keys(password)
+
+        login = self.driver.find_element_by_xpath("//button[text()='Log in']")
+        login.click()
+
+        # two-step auth via email... need to think about how best
+        # to get around this - some potential for automation
+        # currently wait for 2 mins and enter manually
+        try:
+            wait = WebDriverWait(self.driver, 5)
+            cont = wait.until(EC.element_to_be_clickable(
+                (By.XPATH, "//button[text()='Continue']"))
+            )
+            cont.click()
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    with open("/Users/david_zhang/dz_home/work/data_sci/autoceries/credentials.txt") as file:
+        credentials = file.readlines()
+    sb = SainsburyBrowser()
+    sb.shop(credentials[0], credentials[1])
