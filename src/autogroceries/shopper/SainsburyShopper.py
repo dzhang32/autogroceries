3109ddd73c1f8bcb7c733a81a47efca0e6039570
David Zhang <dyzhang32@gmail.com>
2021-10-03 13:57:53 +0100

    add skeleton of Selector and make methods private

--- src/autogroceries/shopper/SainsburyShopper.py
+++ src/autogroceries/shopper/SainsburyShopper.py
@@ -6,19 +6,20 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class SainsburyShopper(Shopper):
+
     def __init__(self):
         sainsbury_url = "https://www.sainsburys.co.uk"
 
         super().__init__(sainsbury_url)
 
     def shop(self, username, password):
-        self.open_sainsbury()
-        self.accept_cookies()
-        self.to_login()
-        self.login(username, password)
-        self.add_to_cart("tomato")
+        self._open_sainsbury()
+        self._accept_cookies()
+        self._to_login()
+        self._login(username, password)
+        self._add_to_cart("tomato")
 
-    def add_to_cart(self, item):
+    def _add_to_cart(self, item):
         search = self.driver.find_element_by_id("search-bar-input")
         search.send_keys(item)
         search = self.driver.find_element_by_xpath(
@@ -34,14 +35,14 @@ class SainsburyShopper(Shopper):
         )
         add.click()
 
-    def open_sainsbury(self):
+    def _open_sainsbury(self):
         self.open_driver()
         self.open_url()
 
         # wait a second for page to load
         time.sleep(1)
 
-    def accept_cookies(self):
+    def _accept_cookies(self):
         # wait for few seconds for the cookies box to become clickable
         wait = WebDriverWait(self.driver, 5)
         accept_box = wait.until(EC.element_to_be_clickable(
@@ -49,7 +50,7 @@ class SainsburyShopper(Shopper):
         )
         accept_box.click()
 
-    def to_login(self):
+    def _to_login(self):
         login = self.driver.find_element_by_xpath("//span[text()='Log in']")
         login.click()
         groceries = self.driver.find_element_by_xpath(
@@ -57,7 +58,7 @@ class SainsburyShopper(Shopper):
         )
         groceries.click()
 
-    def login(self, username, password):
+    def _login(self, username, password):
         self.accept_cookies()
 
         # enter UN and PW
