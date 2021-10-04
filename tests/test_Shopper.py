dab51b1eacfa2d5d929a80792e3e2dc6d03ef4ad
David Zhang <dyzhang32@gmail.com>
2021-10-04 22:36:40 +0100

    change b to shopper to match new class name

--- tests/test_Shopper.py
+++ tests/test_Shopper.py
@@ -3,65 +3,65 @@ from autogroceries.shopper import Shopper
 from selenium.common.exceptions import InvalidSessionIdException
 
 # TODO: check whether I should use @pytest.fixture instead here
-b = Shopper("https://www.sainsburys.co.uk", ["tomato", "lemon"])
+shopper = Shopper("https://www.sainsburys.co.uk", ["tomato", "lemon"])
 
 
 def test_default_shopper():
-    assert b.url == "https://www.sainsburys.co.uk"
-    assert b.headless == False
-    assert b.items == ["tomato", "lemon"]
-    assert b.n_items == [1, 1]
-    assert b.driver is None
+    assert shopper.url == "https://www.sainsburys.co.uk"
+    assert shopper.headless == False
+    assert shopper.items == ["tomato", "lemon"]
+    assert shopper.n_items == [1, 1]
+    assert shopper.driver is None
 
 
 def test_check_headless_catches_user_input_errors():
     with pytest.raises(TypeError, match="headless must be a boolean"):
-        b._check_headless("not_a_bool")
+        shopper._check_headless("not_a_bool")
 
 
 def test_set_headless():
-    b.headless = True
-    assert b.headless == True
+    shopper.headless = True
+    assert shopper.headless == True
 
 
 def test_check_items_catches_user_input_errors():
     with pytest.raises(TypeError, match="items must be a list"):
-        b._check_items(1)
+        shopper._check_items(1)
 
     with pytest.raises(TypeError, match="items must be a list of str elements"):
-        b._check_items([1])
+        shopper._check_items([1])
 
 
 def test_check_n_items_catches_user_input_errors():
     with pytest.raises(TypeError, match="n_items must be a list"):
-        b._check_n_items(b.items, 1)
+        shopper._check_n_items(shopper.items, 1)
 
     with pytest.raises(TypeError, match="n_items must be a list of int"):
-        b._check_n_items(b.items, ["str"])
+        shopper._check_n_items(shopper.items, ["str"])
 
     with pytest.raises(ValueError, match="length of n_items and items"):
-        b.n_items = [1, 2, 3]
+        shopper.n_items = [1, 2, 3]
 
 
 def test_set_n_items():
-    b.n_items = [1, 2]
-    assert b.n_items == [1, 2]
+    shopper.n_items = [1, 2]
+    assert shopper.n_items == [1, 2]
 
 
 def test_open_url_fails_when_driver_not_open():
     with pytest.raises(ValueError, match="driver has not yet been opened"):
-        b._open_url()
+        shopper._open_url()
 
 
 def test_open_url_fails_works_after_open_driver():
-    b._open_driver()
-    b._open_url()
-    assert b.driver.title == "Sainsbury’s"
+    shopper._open_driver()
+    shopper._open_url()
+    assert shopper.driver.title == "Sainsbury’s"
 
 
 def test_close_driver():
     # close the headless browser, could be changed to @pytest.fixture?
-    b.close_driver()
+    shopper.close_driver()
 
     with pytest.raises(InvalidSessionIdException):
-        b.driver.title
+        shopper.driver.title
