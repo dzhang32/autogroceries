c4692dce1b807ea0068c42e28e43f4399391d228
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-08-25 16:23:05 +0100

    feat: go to login page

--- src/autogroceries/shopper.py
+++ src/autogroceries/shopper.py
@@ -1,13 +1,34 @@
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import TimeoutError, sync_playwright
 
 
 class SainsburysShopper:
     URL = "https://www.sainsburys.co.uk"
 
-    def shop(self):
+    def shop(self) -> None:
         with sync_playwright() as p:
+            # TODO: refactor this out into ABC.
             browser = p.chromium.launch(headless=False)
-            page = browser.new_page()
-            
-            page.goto(self.URL)
-            browser.close()
+
+            self.page = browser.new_page()
+            self.page.goto(self.URL)
+
+            self._handle_cookies()
+            self._go_to_login()
+            self._handle_cookies()
+
+    def _go_to_login(self) -> None:
+        self.page.click("text=Log in")
+        self.page.click("text=Groceries account")
+
+    def _handle_cookies(self, timeout: int = 3000) -> None:
+        # Wait 3 seconds for the cookies to load.
+        try:
+            button_selector = "button:has-text('Continue without accepting')"
+            self.page.wait_for_selector(button_selector, timeout=timeout)
+            self.page.click(button_selector)
+        except TimeoutError:
+            # TODO: add logging.
+            pass
+
+    def _login(self, username: str, password: str) -> None:
+        pass
