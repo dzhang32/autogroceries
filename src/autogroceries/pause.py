7712a94910ca50f42b59a23da300e5e6f2523f0d
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-08-25 21:55:50 +0100

    feat: add pause function

---
+++ src/autogroceries/pause.py
@@ -0,0 +1,20 @@
+import functools
+import random
+import time
+from typing import Callable
+
+
+def pause(_func=None, *, delay: int = 1) -> Callable:
+    def decorator_pause(func):
+        @functools.wraps(func)
+        def wrapper_pause(*args, **kwargs):
+            # Add a random delay (up to 0.5 seconds) to emulate a human.
+            time.sleep(delay + random.uniform(0, 0.5))
+            return func(*args, **kwargs)
+
+        return wrapper_pause
+
+    if _func is None:
+        return decorator_pause
+    else:
+        return decorator_pause(_func)
