2fd00cc32e8815deeecc1a913ed588fe9aca0082
David Zhang <32676710+dzhang32@users.noreply.github.com>
2025-09-05 18:02:58 +0100

    test: skip test on CI

--- tests/test_shopper.py
+++ tests/test_shopper.py
@@ -1,8 +1,15 @@
 import os
 
+import pytest
+
 from autogroceries.shopper.sainsburys import SainsburysShopper
 
 
+# GHA autosets GITHUB_ACTIONS env var to true.
+@pytest.mark.skipif(
+    os.environ.get("GITHUB_ACTIONS") == "true",
+    reason="Sainsburys website can't be tested in headless mode.",
+)
 def test_shopper():
     shopper = SainsburysShopper(
         username=os.getenv("SAINSBURYS_USERNAME"),
